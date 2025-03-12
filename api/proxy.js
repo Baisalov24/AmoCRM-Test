1b41490fe8b2b75c8d518900ec4b1ae83083f812
Tima Baisalov <baisalov.t98@gmail.com>
2025-03-11 19:00:02 -0500

    new changes

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-    const proxy = createProxyMiddleware({
-      target: 'https://temirlanbaisalov35.amocrm.ru',
-      changeOrigin: true,
-      pathRewrite: (path) => req.query.path,
-      headers: {
-        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}` // Опционально
+  const proxy = createProxyMiddleware({
+    target: 'https://temirlanbaisalov35.amocrm.ru',
+    changeOrigin: true,
+    pathRewrite: (path) => req.query.path,
+    onProxyReq: (proxyReq, req) => {
+      if (req.body) {
+        proxyReq.setHeader('Content-Type', 'application/json');
+        proxyReq.write(JSON.stringify(req.body));
       }
-    });
-    proxy(req, res);
-  };
+    }
+  });
+  proxy(req, res);
+};
